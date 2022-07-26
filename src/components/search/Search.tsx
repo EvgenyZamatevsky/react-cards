3cb02965bd05cba938ea11aa6dc8412788448700
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-07-26 14:25:13 +0300

    commit

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,14 +1,14 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react'
+import React, { ChangeEvent, FC, useCallback, useRef, useState } from 'react'
 import { ReturnComponentType } from 'types'
 import style from './Search.module.scss'
 import loon from 'assets/icons/loon.svg'
 import cross from 'assets/icons/cross.svg'
-import { useSelector } from 'react-redux'
-import { selectSearchValue } from 'store/selectors'
 import { useAppDispatch } from 'store/hooks'
 import { setSearchValue } from 'store/slices/packs'
-import { useDebounce } from 'hooks/useDebounce'
 import { EMPTY_STRING } from 'constants/base'
+import debounce from 'lodash.debounce'
+import { useSelector } from 'react-redux'
+import { selectIsLoading, selectSearchValue } from 'store/selectors'
 
 type SearchPropsType = {
 
@@ -18,26 +18,43 @@ export const Search: FC<SearchPropsType> = (): ReturnComponentType => {
 
 	const dispatch = useAppDispatch()
 
+	const searchValue = useSelector(selectSearchValue)
+	const isLoading = useSelector(selectIsLoading)
+
 	const [value, setValue] = useState(EMPTY_STRING)
-	const debouncedValue = useDebounce<string>(value, 1000)
+
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const updateSearchValue = useCallback(debounce((value: string) => {
+		dispatch(setSearchValue(value))
+	}, 500), [])
 
 	const onInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
-		setValue(event.currentTarget.value)
+		const currentValue = event.currentTarget.value
+
+		setValue(currentValue)
+		updateSearchValue(currentValue)
 	}
 
-	// const searchValue = useSelector(selectSearchValue)
+	const showInputFocus = (): void => inputRef.current?.focus()
 
-	useEffect(() => {
-		setValue(debouncedValue)
-
-		dispatch(setSearchValue(value))
-	}, [debouncedValue])
+	const onResetSearchValueClick = (): void => {
+		setValue(EMPTY_STRING)
+		dispatch(setSearchValue(EMPTY_STRING))
+		showInputFocus()
+	}
 
 	return (
 		<div className={style.search}>
 			<img className={style.searchIcon} src={loon} />
-			<input className={style.searchInput} placeholder='Search' value={value} onChange={onInputChange} />
-			{3 > 2 && <img className={style.clearIcon} src={cross} alt='cross' />}
+			<input
+				className={style.searchInput}
+				placeholder='Search'
+				value={value}
+				onChange={onInputChange}
+				ref={inputRef} />
+			{!isLoading && searchValue &&
+				<img className={style.clearIcon} onClick={onResetSearchValueClick} src={cross} alt='cross' />}
 		</div>
 	)
 }
