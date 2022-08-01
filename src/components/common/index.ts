eaf4117cb5c94a1df9a4644eeb5e2101984324d1
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-08-01 14:12:52 +0300

    commit

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { ErrorAlert } from './errorAlert'
 export { Preloader } from './preloader'
 export { DoubleRange } from './doubleRange'
+export { EditableItem } from './editableItem'
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { Path } from 'enums'
 import React, { ChangeEvent, FC, useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { ReturnComponentType } from 'types'
 import avatar from 'assets/images/avatar.png'
 import { useAppDispatch } from 'store/hooks'
@@ -9,7 +9,9 @@ import { useSelector } from 'react-redux'
 import { selectAuthorizedUserData, selectIsAuth } from 'store/selectors'
 import { EMPTY_STRING } from 'constants/base'
 import logOutIcon from 'assets/icons/logOut.png'
+import arrow from 'assets/icons/arrow.svg'
 import style from './Profile.module.scss'
+import { EditableItem } from 'components'
 
 type ProfilePropsType = {
 
@@ -30,13 +32,9 @@ export const Profile: FC<ProfilePropsType> = (): ReturnComponentType => {
 	// 	setAuthorizedUserName(event.currentTarget.value)
 	// }
 
-	// const updateAuthorizedUserName = (): void => {
-	// 	const trimmedAuthorizedUserName = authorizedUserName?.trim()
-
-	// 	if (trimmedAuthorizedUserName !== EMPTY_STRING) {
-	// 		dispatch(updateAuthorizedUser({ name: trimmedAuthorizedUserName }))
-	// 	}
-	// }
+	const handleUpdateNameBlurAndKeyDown = (newName: string): void => {
+		dispatch(updateAuthorizedUser({ name: newName }))
+	}
 
 	const onLogOutClick = (): void => {
 		dispatch(logOut())
@@ -48,10 +46,14 @@ export const Profile: FC<ProfilePropsType> = (): ReturnComponentType => {
 
 	return (
 		<div className={style.container}>
+			<Link to={Path.PACKS} className={style.backToPacksListBtn}>
+				<img src={arrow} alt='' />
+				<div>Back to Packs List</div>
+			</Link>
 			<div className={style.content}>
 				<h2 className={style.title}>Personal Information</h2>
 				<img className={style.avatar} src={avatarAuthorizedUser} alt='avatar' />
-				<div className={style.name}>{authorizedUserData?.name}</div>
+				<EditableItem currentValue={authorizedUserData!?.name} changeCurrentValue={handleUpdateNameBlurAndKeyDown} />
 				<div className={style.email}>{authorizedUserData?.email}</div>
 				<button className={style.LogOutBtn} onClick={onLogOutClick}>
 					<img src={logOutIcon} alt='' />
